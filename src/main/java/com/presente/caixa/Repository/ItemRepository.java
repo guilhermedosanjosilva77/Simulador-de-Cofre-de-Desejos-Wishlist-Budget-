295e6acfdf63f6bacb8ffac329bc7249176158cf
Guilherme <dosanjosguilherme88@gmail.com>
2026-02-07 01:25:59 -0300

    Back-End/Adicionando nova lógica de DTO

--- src/main/java/com/presente/caixa/Repository/ItemRepository.java
+++ src/main/java/com/presente/caixa/Repository/ItemRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.presente.caixa.Entity.ItemEntity;
 
-public interface ItemRepository extends JpaRepository <ItemEntity,Long> {
+public interface ItemRepository extends JpaRepository <ItemEntity,Long>  {
 
     
 } 
--- src/main/java/com/presente/caixa/Service/UserService.java
+++ src/main/java/com/presente/caixa/Service/UserService.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.presente.caixa.DTO.UserRequest;
+import com.presente.caixa.DTO.UserResponse;
 import com.presente.caixa.Entity.UserEntity;
 import com.presente.caixa.Repository.UserRepository;
 
@@ -18,43 +20,60 @@ public class UserService {
     }
 
     //CREATE
-    public UserEntity criar(UserEntity userEntity){
+    public UserResponse criar(UserRequest dados,UserEntity userEntity){
         //não permitir a criação de mais de um email
         boolean jaExiste = userRepository.existeEmail(userEntity.getEmail());
         if(jaExiste == true){
             throw new RuntimeException("Endereço de email já existe");
         }
-        return userRepository.save(userEntity);
+
+        //DTO REQUEST, IRÁ REECEBER DO USUÁRIO
+        userEntity.setNome(dados.nome());
+        userEntity.setEmail(dados.email());
+        userEntity.setSenha(dados.senha());
+
+        userRepository.save(userEntity);
+        return new UserResponse(userEntity);
     }
 
     //READ
-    public List<UserEntity> buscar(){
-        return userRepository.findAll();
+    public List<UserResponse> buscar(){
+        return userRepository.findAll().stream().map(UserResponse::new).toList();
     }
 
     //READ BY ID
-    public UserEntity buscarPorId(Long id_user){
-        return userRepository.findById(id_user).orElse(null);
+    public UserResponse buscarPorId(Long id_user){
+        UserEntity usuario =userRepository.findById(id_user).orElse(null);
+        if (usuario != null) {
+            return new UserResponse(usuario);
+            
+        }
+        else{
+            return null;
+        }
     }
 
     //UPDATE
-    public UserEntity atualizar(Long id_user, UserEntity atualizar){
-        UserEntity userEntity = buscarPorId(id_user);
+    public UserResponse atualizar(Long id_user, UserRequest atualizar){
+        UserEntity userEntity = userRepository.findById(id_user).orElse(null);
 
         if (userEntity == null) {
             return null;
             
         }
         
-        userEntity.setNome(atualizar.getNome());
-        userEntity.setEmail(atualizar.getEmail());
-        userEntity.setSenha(atualizar.getSenha());
+        userEntity.setNome(atualizar.nome());
+        userEntity.setEmail(atualizar.email());
+        userEntity.setSenha(atualizar.senha());
 
-        return userRepository.save(userEntity);
-    }    
+         userRepository.save(userEntity);
+         return new UserResponse(userEntity);
+    }   
+
 
     //DELETE
     public void deletar(Long id_user){
         userRepository.deleteById(id_user);
     }
+    //TUDO QUE RECEBE UM OBJETO DE ENTITY PRECISA PASSAR PELO DTO
 }
